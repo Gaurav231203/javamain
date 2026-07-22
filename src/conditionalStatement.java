3da8a83fa9deaf620c3b1d1379ef424c89c69c8d
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-22 16:33:51 +0530

    Added new files

---
+++ src/conditionalStatement.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class conditionalStatement {
+    public static  void  main(String[] args){
+
+        // if statement ====> it is used for checks block of statement or code and return the output
+        // if the conditions is true
+        int a =10;
+        int b= 20;
+        if (a>b){
+            System.out.println("a is bigger= " + a);
+        }
+        else{
+            System.out.println(" b is bigger= " + b);
+        }
+
+    }
+}
---
+++ src/randomNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class randomNumbers {
+    public static void main(String[] args){
+        Random random =new Random();
+
+        //int number;
+
+       // number = random.nextInt(1000, 9999);
+
+       // System.out.println(number);
+     //=======> for boolean numb
+
+        boolean isHead;
+
+        isHead =  random.nextBoolean();
+        System.out.println(isHead);
+
+    }
+}
